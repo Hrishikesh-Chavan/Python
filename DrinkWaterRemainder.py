0ed4c9e46dcc10bb28731e49cbb9ff4527ca8397
Hrishikesh-Chavan <96835309+Hrishikesh-Chavan@users.noreply.github.com>
2022-03-11 19:00:27 +0530

    Create DrinkWaterRemainder.py

---
+++ DrinkWaterRemainder.py
@@ -0,0 +1,13 @@
+import time
+from plyer import notification
+
+if __name__ == "__main__":
+    while True:
+        notification.notify(
+            title = "Drink Water",
+            app_icon = "D:\Programming\Cool Programs\Drink Water Remainder\icon.ico",
+            message = "Stay hydrated !! it helps to cool your mind, preventing constipation, stabilizing the heartbeat, hydrate Skin,, look cool, flushing bacteria from your bladder.",
+            timeout = 5
+        )
+        time.sleep(60 * 60)
+
