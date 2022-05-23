b8f22d2b9ad518b410c01b6105570ac908f417d2
Hrishikesh-Chavan <96835309+Hrishikesh-Chavan@users.noreply.github.com>
2022-05-23 20:20:09 +0530

    Update DrinkWaterRemainder.py

--- DrinkWaterRemainder.py
+++ DrinkWaterRemainder.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     while True:
         notification.notify(
             title = "Drink Water",
-            app_icon = "D:\Programming\Cool Programs\Drink Water Remainder\icon.ico",
+            app_icon = "D:\Programming\Cool Programs\Drink Water Remainder\icon.ico", # Change directory accorfing to icon folder.
             message = "Stay hydrated !! it helps to cool your mind, preventing constipation, stabilizing the heartbeat, hydrate Skin,, look cool, flushing bacteria from your bladder.",
             timeout = 5
         )
